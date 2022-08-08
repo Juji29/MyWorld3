75b6b938370f4d5aa16d13872bcf57c736238ddf
Juji29 <julien.legavre@alumni.enac.fr>
2022-08-08 11:05:28 +0200

    modification 08/08 11:00

---
+++ MyWorld3Modified/world3_dynamic.py
@@ -0,0 +1,217 @@
+import numpy as np
+from scipy.integrate import solve_ivp, odeint
+import matplotlib.pyplot as plt
+
+C = "CONSTANT"
+CT = "CONSTANT_TABLE"
+
+
+class ErrorNodeAlreadyExists(Exception): pass
+
+
+class Node:
+    def __init__(self, name, val=None, hg=None):
+        self.name = name  # string
+        self.val = val    # float
+        self.cons = None  # fonction
+        self.pred = []    # predecesseurs
+        self.succ = []    # successeurs
+        self.rank = None  # rang
+        if hg is not None: # ajout du noeud à l'hypergraphe hg
+            hg.add_node(self)
+
+    def __repr__(self):
+        value = "None"
+        if self.val != None:
+            value = "{:.03f}".format(self.val)
+        return "{0.name:<8} {3} IN: {1:<20} OUT: {2} HIST: {4}".format(self, ",".join(self.get_pred_name()), ",".join(self.get_succ_name()), value, self.hist)
+
+    def set_cons(self, f, pred):
+        self.cons = f
+        self.pred = pred
+        for p in pred:
+            p.succ.append(self)
+
+    def get_pred_name(self):
+        return [p.name for p in self.pred]
+
+    def get_succ_name(self):
+        return [p.name for p in self.succ]
+
+
+class NodeStock(Node):
+    def __init__(self, name, val=None, hg=None):
+        super().__init__(name, val, hg)
+        self.hist = [val]
+
+    def eval(self, ts, save=True):
+        #text = "{}:{}->".format(self.name, self.val)
+        if self.val:
+            self.val = self.val + self.cons(*[p.val for p in self.pred]) * ts
+        else:
+            self.val = self.cons(*[p.val for p in self.pred]) * ts
+        if save:
+            self.hist.append(self.val)
+        #text += "{}".format(self.val)
+        #print(text)
+
+
+class NodeFlow(Node):
+    def __init__(self, name, val=None, hg=None):
+        super().__init__(name, val, hg)
+        self.hist = []
+
+    def eval(self, dt, save=True):
+        #text = "{}:{}->".format(self.name, self.val)
+        self.val = self.cons(*[p.val for p in self.pred])
+        if save:
+            self.hist.append(self.val)
+        #text += "{}".format(self.val)
+        #print(text)
+
+class NodeSmooth(Node):
+    def __init__(self, name, t, size, val=None, hg=None):
+        super().__init__(name, val, hg)
+        self.type = t
+        self.hist = [None] * size
+        self.cst = None
+        self.node = None
+        self.k = 0
+        if t == "DELAY3":
+            self.I1 = None
+            self.histI1 = [None] * size
+            self.I2 = None
+            self.histI2 = [None] * size
+            self.I3 = None
+            self.histI3 = [None] * size
+
+    def eval(self, ts, save=True):
+        #text = "{}:{}->".format(self.name, self.val)
+        self.cons(*[p.val for p in self.pred])
+        if self.type == "SMOOTH":
+            self.val += (self.node - self.val) * ts / self.cst
+        if self.type == "DELAY3":
+            dl = self.cst / 3
+            RT1 = self.I1 / dl
+            self.I1 = self.I1 + (self.node - RT1) * ts
+            RT2 = self.I2 / dl
+            self.I2 = self.I2 + (RT1 - RT2) * ts
+            self.I3 = self.I3 + (RT2 - self.I3 / dl) * ts
+            self.val = self.I3 / dl
+        if save:
+            k = self.k
+            self.hist[k] = self.val
+            if self.type == "DELAY3":
+                self.histI1[k] = self.I1
+                self.histI2[k] = self.I2
+                self.histI3[k] = self.I3
+            self.k += 1
+        #text += "{}".format(self.val)
+        #print(text)
+
+    def __repr__(self):
+        value = "None"
+        if self.val != None:
+            value = "{:.03f}".format(self.val)
+        return "{0.name:<8} {3} IN: {1:<20} OUT: {2}".format(self, ",".join(self.get_pred_name()),
+                                                             ",".join(self.get_succ_name()), value)
+
+    def f_smooth(self, flow, constant):
+        self.node = flow
+        self.cst = constant
+        if not self.hist[0]:
+            if self.type == "SMOOTH":
+                self.val = flow
+            if self.type == "DELAY3":
+                self.val = flow
+                self.I1 = self.I2 = self.I3 = flow * constant / 3
+
+
+class NodeConstant(Node):
+    def __init__(self, name, t, val=None, hg=None):
+        super().__init__(name, val, hg)
+        self.type = t
+
+    def __repr__(self):
+        value = "None"
+        if self.val is not None:
+            if self.type == CT:
+                l1 = ",".join([str(x) for x in self.val[0]])
+                l2 = ",".join([str(x) for x in self.val[1]])
+                value = "[{}]:[{}]".format(l1, l2)
+            else:
+                value = "{0.val:.03f} ".format(self)
+        return "{0.name:<8} {3} IN: {1:<20} OUT: {2}".format(self, ",".join(self.get_pred_name()), ",".join(self.get_succ_name()), value)
+
+
+class Hypergraph():
+    def __init__(self, nodes=[]):
+        self.nodes = {n.name: n for n in nodes}
+        self.nbrank = None
+        self.nodesrank = None
+        self.stocks = [n for n in nodes if type(n) == NodeStock]
+
+    def __repr__(self):
+        return "\n".join([str(v) for c,v in self.nodes.items()])
+
+    def add_node(self, node):
+        if node.name in self.nodes:
+            print("Le Node {} existe déjà. Modifier la modélisation :".format(node.name))
+            print(self.nodes[node.name])
+            raise ErrorNodeAlreadyExists()
+        self.nodes[node.name] = node
+        if type(node) == NodeStock: self.stocks.append(node)
+
+    def add_nodes(self, nodes):
+        for n in nodes:
+            self.add_node(n)
+
+    def add_edge(self, f, x_target, x_s):
+        x_target.set_cons(f, x_s)
+
+    def eval(self, ts):
+        for ns in self.nodesrank:
+            ns.eval(ts)
+
+    def run(self, nbpas, ts):
+        for i in range(nbpas):
+            self.eval(ts)
+        for stock in self.stocks:
+            stock.hist.pop()
+
+    def sub_graph_vertex(self, cond):
+        d2 = [name for name, n in self.nodes.items() if cond(n)]
+        d1 = {name : i for i, name in enumerate(d2)}
+        size = len(d2)
+        gM = [[] for _ in range(size)]
+        gP = [[] for _ in range(size)]
+        for name in d2:
+            n = self.nodes[name]
+            for u in n.pred:
+                if cond(u):
+                    gM[d1[name]].append(d1[u.name])
+                    gP[d1[u.name]].append(d1[name])
+        return d2, gM, gP
+        
+    def set_rank(self):
+        d2, gM, gP = self.sub_graph_vertex(lambda x: type(x) == NodeSmooth or (type(x) == NodeFlow and not x.val))
+        d2inflow, gMinflow, gPinFlow = self.sub_graph_vertex(lambda x: type(x) == NodeFlow and x.val)
+        size = len(d2)
+        dM = [len(gi) for gi in gM]
+        S0 = [i for i,di in enumerate(dM) if di == 0]
+        r = [None]*size
+        def rang_rec(Sk, k):
+            Sk1 = []
+            for i in Sk:
+                r[i] = k
+                for j in gP[i]:
+                    dM[j] -= 1
+                    if dM[j] == 0:
+                        Sk1.append(j)
+            if len(Sk1) > 0 :
+                return rang_rec(Sk1, k+1)
+        rang_rec(S0, 0)
+        self.nbrank = max(r) + 1
+        self.nodesrank = [self.nodes[d2[j]] for _,j in sorted([(ri, i) for i, ri in enumerate(r)])]
+        self.nodesrank += [self.nodes[d2inflow[j]] for j in range(len(d2inflow))]
+        self.nodesrank += self.stocks
---
+++ MyWorld3Modified/world3_run.py
@@ -0,0 +1,147 @@
+import world3_graph as w3
+from world3_plot import *
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    #########
+    # Solve #
+    #########
+    w3.h.set_rank()
+    w3.h.run(w3.nbpas, w3.TS.val)
+
+#################################
+# Reading and analysing results #
+#################################
+ppolx_ref = read("Resultats_ref/Pyworld3/ppolx")
+pop_ref = read("Resultats_ref/Pyworld3/pop")
+fpc_ref = read("Resultats_ref/Pyworld3/fpc")
+iopc_ref = read("Resultats_ref/Pyworld3/iopc")
+nrfr_ref = read("Resultats_ref/Pyworld3/nrfr")
+
+fioaa_ref = read("Resultats_ref/Pyworld3/fioaa")
+aiph_ref = read("Resultats_ref/Pyworld3/aiph")
+tai_ref = read("Resultats_ref/Pyworld3/tai")
+io_ref = read("Resultats_ref/Pyworld3/io")
+fcaor_ref = read("Resultats_ref/Pyworld3/fcaor")
+
+lmf_ref = read("Resultats_ref/Pyworld3/lmf")
+al_ref = read("Resultats_ref/Pyworld3/al")
+ly_ref = read("Resultats_ref/Pyworld3/ly")
+
+ai_ref = read("Resultats_ref/Pyworld3/ai")
+
+delta_ppolx = comparaison(w3.ppolx.hist, ppolx_ref)
+delta_pop = comparaison(w3.pop.hist, pop_ref)
+delta_fpc = comparaison(w3.fpc.hist, fpc_ref)
+delta_iopc = comparaison(w3.iopc.hist, iopc_ref)
+delta_nrfr = comparaison(w3.nrfr.hist, nrfr_ref)
+
+delta_fioaa = comparaison(w3.fioaa.hist, fioaa_ref)
+delta_aiph = comparaison(w3.aiph.hist, aiph_ref)
+delta_tai = comparaison(w3.tai.hist, tai_ref)
+delta_io = comparaison(w3.io.hist, io_ref)
+delta_fcaor = comparaison(w3.fcaor.hist, fcaor_ref)
+
+delta_lmf = comparaison(w3.lmf.hist, lmf_ref)
+delta_al = comparaison(w3.al.hist, al_ref)
+delta_ly = comparaison(w3.ly.hist, ly_ref)
+
+delta_ai = comparaison(w3.ai.hist, ai_ref)
+
+# ploting and printing gaps between real results and historical ones
+label_gap = ["Delta POP", "Delta FPC", "Delta IOPC", "Delta NRFR", "Delta FIOAA", "Delta AIPH", "Delta TAI",
+             "Delta IO", "Delta FCAOR", "Delta LMF", "Delta AL", "Delta LY"]
+sol_gap = [delta_pop, delta_fpc, delta_iopc, delta_nrfr, delta_fioaa, delta_aiph, delta_tai, delta_io, delta_fcaor,
+           delta_lmf, delta_al, delta_ly]
+sol_min_gap = [min(elt) for elt in sol_gap]
+sol_max_gap = [max(elt) for elt in sol_gap]
+afficherreur(w3.t.hist, sol_gap, label_gap)
+gap_tot = 0
+for var in sol_gap:
+    for i in var:
+        gap_tot += abs(i)
+print(gap_tot/200/12)
+
+
+######################
+# Plots of variables #
+######################
+params = {'lines.linewidth': '3'}
+plt.rcParams.update(params)
+#standard_run_overview
+stand_aff(w3.t.hist,
+            [w3.nrfr.hist, w3.iopc.hist, w3.fpc.hist, w3.pop.hist, w3.ppolx.hist],
+            ["NRFR", "IOPC", "FPC", "POP", "PPOLX"],
+            [[0, 1], [0, 1e3], [0, 1e3], [0, 16e9], [0, 32]],
+            img_background="background/overview.png",
+            grid=1,
+            title="World3 standard run")
+
+#standard_run_capital_sector
+stand_aff(w3.t.hist,
+            [w3.fcaor.hist, w3.io.hist, w3.tai.hist, w3.aiph.hist, w3.fioaa.hist],
+            ["FCAOR", "IO", "TAI", "AIPH", "FIOAA"],
+            [[0, 1], [0, 4e12], [0, 4e12], [0, 200], [0, 0.201]],
+            img_background="background/capital.png",
+            grid=1,
+            title="World3 standard run Capital sector")
+
+#standard_run_agriculture_sector
+stand_aff(w3.t.hist,
+            [w3.ly.hist, w3.al.hist, w3.fpc.hist, w3.lmf.hist, w3.pop.hist],
+            ["LY", "AL", "FPC", "LMF", "POP"],
+            [[0, 4e3], [0, 4e9], [0, 800], [0, 1.6], [0, 16e9]],
+            img_background="background/agriculture.png",
+            grid=1,
+            title="World3 standard run Agricultural sector")
+
+################################
+# Comparaison Limits To Growth #
+################################
+#Scenario1
+stand_aff(w3.t.hist,
+            [w3.nrfr.hist, w3.io.hist, w3.f.hist, w3.pop.hist, w3.ppolx.hist],
+            ["NRFR", "IO", "F", "POP", "PPOLX"],
+            [[0, 2], [0, 4e12], [0, 6e12], [0, 10e9], [0, 37]],
+            img_background="background/scenario1-1.png",
+            grid=1,
+            title="World3 Scenario 1 State of the World")
+
+stand_aff(w3.t.hist,
+          [w3.fpc.hist, w3.le.hist, w3.sopc.hist, w3.aiopc.hist],
+          ["FPC", "LE", "SOPC", "AIOPC"],
+          [[0, 1e3], [0, 90], [0, 1.1e3], [0, 600]],
+            img_background="background/scenario1-2.png",
+            grid=1,
+            title="World3 Scenario 1 Material Standard of Living")
+
+stand_aff(w3.t.hist,
+          [w3.hwi.hist, w3.hef.hist],
+          ["HWI", "HEF"],
+          [[0, 0.9], [0, 4.5]],
+            img_background="background/scenario1-3.png",
+            grid=1,
+            title="World3 Scenario 1 Human Welfare and Footprint")
+
+#Scenario2
+stand_aff(w3.t.hist,
+            [w3.pop.hist, w3.iopc.hist, w3.nrfr.hist, w3.ppolx.hist, w3.fpc.hist],
+            ["POP", "IOPC", "NRFR", "PPOLX", "FPC"],
+            [[0, 16e9], [0, 1e3], [0, 1], [0, 32], [0, 1e3]],
+            img_background=None,
+            grid=1,
+            title="World3 Scenario 2 State of the World")
+stand_aff(w3.t.hist,
+            [w3.fpc.hist, w3.le.hist, w3.sopc.hist],
+            ["FPC", "LE", "SOPC"],
+            [[0, 1e3], [0, 100], [0, 1e3]],
+            img_background=None,
+            grid=1,
+            title="World3 Scenario 2 Material Standard of Living")
+stand_aff(w3.t.hist,
+            [w3.hwi.hist, w3.hef.hist],
+            ["HWI", "HEF"],
+            [[0, 1], [0, 5]],
+            img_background=None,
+            grid=1,
+            title="World3 Scenario 2 Human Welfare and Footprint")
